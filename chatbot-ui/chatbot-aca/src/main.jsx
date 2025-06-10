50716ab61c8533dc2cd5765e70c3ccb7d9ae6a63
ennajari <abdellahennajari2018@gmail.com>
2025-06-10 12:22:27 +0200

    vllm deploy

--- chatbot-ui/chatbot-aca/src/main.jsx
+++ chatbot-ui/chatbot-aca/src/main.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { registerSW } from 'virtual:pwa-register'
 
-// Register service worker for PWA
-if ('serviceWorker' in navigator) {
-  registerSW()
-}
+// Register service worker for PWA (commenté temporairement)
+// if ('serviceWorker' in navigator) {
+//   registerSW()
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
