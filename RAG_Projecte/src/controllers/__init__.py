af9cde6f33074e3f7024fbcd0273014134d32653
Oussama EL-HADJI <oussousselhadji@gmail.com>
2026-08-09 04:22:29 +0100

    refactor(architecture): remove obsolete prototype folders (RAG, RAG_Projecte, delpoy_code_to_modal) and unify Modal deployment under deploy_code

--- RAG_Projecte/src/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-#utilisant ce fichier pour la simplifications des importations
-from .DataController import DataController
-from .ProjectController import ProjectController
-from .ProcessController import ProcessController
-from .NLPController import NLPController
